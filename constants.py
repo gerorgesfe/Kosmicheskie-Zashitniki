e0ba6253d69fcc1e4a7c283a694e002241abc2e7
gerorgesfe <gerorgelev@gmail.com>
2026-02-28 09:23:50 +0300

    constants.py

--- constants.py
+++ constants.py
@@ -4,14 +4,19 @@ SCREEN_HEIGHT = 600
 SCREEN_TITLE = "KosmicheskieZashitniki"
 
 # 2. Физика и движение
-ENEMY_BASE_SPEED = -1
-BULLET_BASE_SPEED = 5
+COFFICENT_SPEED_ENEMY = 1
+COFFICENT_SPEED_BULLET_PLAYER = 1
+COFFICENT_SPEED_BULLET_ENEMY = 1
+ENEMY_BASE_SPEED = -1 * COFFICENT_SPEED_ENEMY
+BULLET_BASE_SPEED = 5 * COFFICENT_SPEED_BULLET_PLAYER
 ENEMY_SHOOT_BASE_INTERVAL = 90
-SHOT_COOLDOWN = 0.1
+SHOT_COOLDOWN = 0.1 * COFFICENT_SPEED_BULLET_ENEMY
 
 # 3. Здоровье и характеристики сущностей
-TOUGH_ENEMY_HEALTH = 3
+COFFICENT_HEALTH = 1
+TOUGH_ENEMY_HEALTH = 3 * COFFICENT_HEALTH
 WALL_DESTR_HEALTH = 3
+PLAYER_LIVES = 1
 
 # 4. Система очков и прогрессия сложности
 SCORE_PER_ENEMY = 10
@@ -32,7 +37,8 @@ COST_WALL_DESTR = 5
 COST_WALL_PASS = 4
 
 # 5. Система волн и бюджет спавна
-BASE_WAVE_BUDGET = 15
+COFFICENT_BUDGET = 1
+BASE_WAVE_BUDGET = 15 * COFFICENT_BUDGET
 BUDGET_INCREASE_PER_WAVE = 5
 MAX_WALL_PERCENT = 0.2
 WAVE_COOLDOWN_FRAMES = 120
@@ -48,6 +54,12 @@ TEX_TOUGH_ENEMY = "images/x-wing_enemy.png"
 TEX_WALL = "images/Wall.png"
 TEX_WALL_DESTR = "images/Wall_shoot.png"
 TEX_WALL_PASS = "images/Wall_pass.png"
+POWER_CARD = "images/power_card.png"
+SPEED_CARD = "images/speed_.png"
+DAMAGE_CARD = "images/damage_card.png"
+MONEY_CARD = "images/money_.png"
+UNBUG_CARD = "images/Unbug_card.png"
+DEBUG_CARD = "images/debug_card.png"
 
 # 8. Данные и сохранение
 DB_NAME = "data/game_data.db"
